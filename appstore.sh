5704f5f67800294bba8357a32485acfa6d38e63c
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-02-09 17:32:32 +0900

    Mac App Storeのインスコを自動化

---
+++ appstore.sh
@@ -0,0 +1,11 @@
+#
+# Mac App Store apps install
+#
+echo "------ Mac App Store apps -----"
+brew install mas
+mas install 539883307  # LINE(4.11.1)
+mas install 803453959  # Slack (2.4.1)
+mas install 409183694  # Keynote (6.6.2)
+mas install 409203825  # Numbers (3.6.2)
+mas install 409201541  # Pages (5.6.2)
+echo "----- END -----"
